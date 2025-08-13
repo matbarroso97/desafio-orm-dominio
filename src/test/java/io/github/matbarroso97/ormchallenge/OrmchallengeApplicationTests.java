bc43f384e7c2a9ccd86281f7d441e872fc849eb2
Matheus <matheusbarrosohomeoffice@gmail.com>
2025-08-13 12:40:27 -0300

    Enviando desafio

---
+++ src/test/java/io/github/matbarroso97/ormchallenge/OrmchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.matbarroso97.ormchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
